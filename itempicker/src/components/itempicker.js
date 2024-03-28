328bd41e5fa635406da47f3c7348547a4dda89b3
Nico Felten <n_felten@hotmail.com>
2024-03-28 14:34:09 +0100

    images in columns
    images in columns

--- itempicker/src/components/itempicker.js
+++ itempicker/src/components/itempicker.js
@@ -1,24 +1,38 @@
 import "./itempicker.css";
-import Logo from './Logo.png';
+import "./tooltiptext.css";
 //import data from './items';
+ const data = require('./items.json');
 
-const data = require('./items.json');
+function ItemPicker() {
+    let Counter = 0;
+    let MaxCounter = 7;
 
-function ItemPicker()
-{
     return (
-        <>
+        <div id="Table">
             {data.map( (item) => {
-                return (
-                    <div key={item.id}>
-                        <div><img class = "Image" style={{width: "50px", height: "50px"}} src={"./items/" + item.icon} alt="item"/></div>
-                        <div>{item.name}</div>
-                        <hr></hr>
-                    </div>
-                );
-            })}
-        </>
-    );
+                Counter=Counter + 1;
+                if (Counter===MaxCounter)
+                 {
+                    Counter=0;
+                    return (
+                        <>
+                        <div className="tooltip" key={item.id}>
+                            <img className="Image" src={"./items/" + item.icon} alt="item" />
+                            <span className="tooltiptext">{item.name}<br></br>{item.tags}</span>
+                        </div>
+                        <div className="newline"></div>
+                        </>
+                        )
+                    } else
+                    {return (
+                        <div className="tooltip" key={item.id}>
+                            <img className="Image" src={"./items/" + item.icon} alt="item" />
+                            <span className="tooltiptext">{item.name}<br></br>{item.tags}</span>
+                        </div>);
+                    };
+            }   )}
+        </div>
+    )
 }
 
- export default ItemPicker;
+export default ItemPicker;
